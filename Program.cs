565994b2529f30768a02ef5e6e6abbfd691976da
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-15 11:37:13 +0530

    Logical Programming

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LogicalFunction
+{
+    public class program
+    {
+        public static void Main(String[] args)
+        {
+            StopWatch.StopWatchProgram();
+            //Coupon.CouponNumber();
+            //PrimeNumber.CheckPrimeNumber();
+            // Reversenumber.CheckReversenumber();
+            //PerfectNumber.FindPerfectNumber();
+            //Fibonacci.FindFibonacci();
+        }
+    }
+}
